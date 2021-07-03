7ebb284802980610f6f73d1ea8cd63d23cfb16c1
Tõnis Prants <tonis.prants7@gmail.com>
2021-07-03 14:46:59 +0300

    removed dummy start and placeholder folder

--- src/main/java/hedgehogs/strategyGame/StrategyGameApplication.java
+++ src/main/java/hedgehogs/strategyGame/StrategyGameApplication.java
@@ -1,6 +1,5 @@
 package hedgehogs.strategyGame;
 
-import hedgehogs.strategyGame.gameLogic.placeholderLocation.DummyStart;
 import hedgehogs.strategyGame.javaSwingInterface.mainWindowBooter.MainWindowFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
