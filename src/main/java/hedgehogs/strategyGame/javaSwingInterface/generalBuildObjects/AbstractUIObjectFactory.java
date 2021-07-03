38e32a218eecc2bd317dd32a25e40aacf8489588
Tõnis Prants <tonis.prants7@gmail.com>
2021-07-03 15:24:55 +0300

    Made basic minor element inheritance classes and removed unused listeners

---
+++ src/main/java/hedgehogs/strategyGame/javaSwingInterface/generalBuildObjects/AbstractUIObjectFactory.java
@@ -0,0 +1,40 @@
+package hedgehogs.strategyGame.javaSwingInterface.generalBuildObjects;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class AbstractUIObjectFactory implements UIObjectFactory {
+    private JPanel mainPanel;
+    private GridBagConstraints layout;
+
+    private void bootUp() {
+        this.mainPanel = new JPanel();
+        this.layout = new GridBagConstraints();
+        this.layout.fill = GridBagConstraints.HORIZONTAL;
+        this.makeAllMinorElements();
+    }
+
+    @Override
+    public JPanel getPanelObject() {
+        if(this.mainPanel == null) {
+            this.bootUp();
+        }
+        return this.mainPanel;
+    }
+
+    @Override
+    public void refreshElements() {
+        this.elementContentRefresh();
+    }
+
+    protected abstract void makeAllMinorElements();
+
+    protected abstract void elementContentRefresh();
+
+    protected boolean addNewElementToPanel(Component newComponent, int xLocation, int yLocation) {
+        this.layout.gridx = xLocation;
+        this.layout.gridy = yLocation;
+        this.mainPanel.add(newComponent, this.layout);
+        return true;
+    }
+}
---
+++ src/main/java/hedgehogs/strategyGame/javaSwingInterface/generalBuildObjects/UIObjectFactory.java
@@ -0,0 +1,10 @@
+package hedgehogs.strategyGame.javaSwingInterface.generalBuildObjects;
+
+import javax.swing.*;
+
+public interface UIObjectFactory {
+
+    public JPanel getPanelObject();
+
+    public void refreshElements();
+}
