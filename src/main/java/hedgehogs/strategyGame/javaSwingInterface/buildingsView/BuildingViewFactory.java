4d005ad5c13148aa47475d9b552aa43f285a34f5
Tõnis Prants <tonis.prants7@gmail.com>
2021-07-24 13:10:23 +0300

    building view now displays all building slots

--- src/main/java/hedgehogs/strategyGame/javaSwingInterface/buildingsView/BuildingViewFactory.java
+++ src/main/java/hedgehogs/strategyGame/javaSwingInterface/buildingsView/BuildingViewFactory.java
@@ -3,23 +3,27 @@ package hedgehogs.strategyGame.javaSwingInterface.buildingsView;
 import hedgehogs.strategyGame.gameLogic.factions.Faction;
 import hedgehogs.strategyGame.gameLogic.factions.FactionPhoneBook;
 import hedgehogs.strategyGame.gameLogic.land.Province;
+import hedgehogs.strategyGame.gameLogic.land.buildings.buildingSlots.BuildingSlot;
 import hedgehogs.strategyGame.javaSwingInterface.buildingsView.buildSlotMinorView.BuildSlotMinorView;
 import hedgehogs.strategyGame.javaSwingInterface.generalBuildObjects.AbstractUIObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class BuildingViewFactory extends AbstractUIObjectFactory {
     private Province lastSelectedProvince;
     private JLabel provinceNameLabel;
-    private BuildSlotMinorView trialSlot;
+    private List<BuildSlotMinorView> buildSlots;
     private FactionPhoneBook factionPhoneBook;
 
     @Autowired
     public BuildingViewFactory(FactionPhoneBook factionPhoneBook) {
         this.factionPhoneBook = factionPhoneBook;
+        this.buildSlots = new ArrayList<>();
     }
 
     @Override
@@ -34,12 +38,24 @@ public class BuildingViewFactory extends AbstractUIObjectFactory {
 
     @Override
     protected void elementContentRefresh() {
+        this.getPanelObject().removeAll();
+        this.makeNameLabel();
         this.writeNameLabel();
-        this.trialSlot = new BuildSlotMinorView(
-                this.factionPhoneBook.getPlayerFaction(),
-                this.lastSelectedProvince.accessBuildings().getAllSlots().get(0));
-        this.addNewElementToPanel(this.trialSlot.getPanelObject(), 0, 1);
-        this.trialSlot.refreshElements();
+        this.addAllBuildingSlotViews();
+    }
+
+    private void addAllBuildingSlotViews() {
+        int y = 0;
+        for(BuildingSlot oneSlot : this.lastSelectedProvince.accessBuildings().getAllSlots()) {
+            y++;
+            this.addAnotherSlot(oneSlot, y);
+        }
+    }
+
+    private void addAnotherSlot(BuildingSlot targetSlot, int yCoordinate) {
+        BuildSlotMinorView newBuildingSlotView = new BuildSlotMinorView(this.factionPhoneBook.getPlayerFaction(), targetSlot);
+        this.addNewElementToPanel(newBuildingSlotView.getPanelObject(), 0, yCoordinate);
+        newBuildingSlotView.refreshElements();
     }
 
     private void writeNameLabel() {
